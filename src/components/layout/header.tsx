1e394c4a37d37abd536208e46a24f11fe907d962
francisco ruales pianda <franciscorualesp15@gmail.com>
2025-06-02 21:58:35 -0500

    eliminar estilo molesto

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -15,7 +15,7 @@ export function Header({ className, title = "Hotel San Miguel" }: HeaderProps) {
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex-1">
           <Link href="/dashboard">
-            <h1 className="inline text-xl font-bold bg-gradient-to-r from-primary to-primary/70 bg-clip-text text-transparent">
+            <h1 className="inline text-xl font-bold bg-gradient-to-r from-primary to-primary/70 bg-clip-text">
               {title}
             </h1>
           </Link>
